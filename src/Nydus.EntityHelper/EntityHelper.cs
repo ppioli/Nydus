f1c9d1b99783278b148925426e467e1c718397f2
Pablo Pioli <pablopioli@gmail.com>
2022-03-17 19:31:30 -0300

    Modified EntityHelper to be less dependant on the context

--- src/Nydus.EntityHelper/EntityHelper.cs
+++ src/Nydus.EntityHelper/EntityHelper.cs
@@ -7,20 +7,24 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Nydus.EntityHelper;
 
-public abstract class EntityHelper<TEntity, TUser, TUserKey> : MapperHelper<TEntity>, IEntityHelper<TEntity, TUser, TUserKey>
+public class EntityHelper<TEntity, TUser, TUserKey> : MapperHelper<TEntity>, IEntityHelper<TEntity, TUser, TUserKey>
     where TEntity : class, IEntity<TUser, TUserKey>, new()
     where TUser : IdentityUser<TUserKey>
     where TUserKey : IEquatable<TUserKey>
 {
-    protected readonly IHttpContextAccessor ContextAccessor;
+    private readonly TUser? _user;
     
     public override IQueryable<TEntity> Entities => base.Entities.Where(s => !s.SoftDeleted);
     
-    protected EntityHelper(DbContext dbContext, IMapper mapper, IHttpContextAccessor contextAccessor) : base(
+    protected EntityHelper(
+        DbContext dbContext,
+        IMapper mapper,
+        IHttpContextAccessor contextAccessor,
+        Func<HttpContext, TUser> getLoggedUser) : base(
         dbContext,
         mapper)
     {
-        ContextAccessor = contextAccessor;
+        _user = contextAccessor.HttpContext == null ? null : getLoggedUser(contextAccessor.HttpContext!);
     }
 
     public override TEntity? TryFind(object id)
@@ -32,35 +36,32 @@ public abstract class EntityHelper<TEntity, TUser, TUserKey> : MapperHelper<TEnt
 
     protected override void HandleUpdate(TEntity e)
     {
-        var user = GetLoggedUser();
-
+        if (_user == null)
+        {
+            throw new Exception("No logged user attempting to modify entities");
+        }
         e.UpdatedAt = DateTime.Now;
-        e.UpdatedBy = user;
-        e.UpdatedById = user.Id;
+        e.UpdatedBy = _user;
+        e.UpdatedById = _user.Id;
     }
 
     protected override void HandleCreate(TEntity e)
     {
-        var user = GetLoggedUser();
+        if (_user == null)
+        {
+            throw new Exception("No logged user attempting to modify entities");
+        }
+        
         e.UpdatedAt = DateTime.Now;
-        e.UpdatedById = user.Id;
-        e.UpdatedBy = user;
+        e.UpdatedById = _user.Id;
+        e.UpdatedBy = _user;
         e.CreatedAt = DateTime.Now;
-        e.CreatedById = user.Id;
-        e.CreatedBy = user;
+        e.CreatedById = _user.Id;
+        e.CreatedBy = _user;
     }
 
     public override void HandleDelete(TEntity e)
     {
         e.SoftDeleted = true;
     }
-
-    protected HttpContext GetHttpContext()
-    {
-        if (ContextAccessor?.HttpContext == null) throw new Exception("The http context was not initialized on the entity helper");
-
-        return ContextAccessor?.HttpContext!;
-    }
-
-    public abstract TUser GetLoggedUser();
 }
